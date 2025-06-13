b205e5cbea1e38eb730ae1e6be398f0ebf418672
DBFerPra <82104023+DBFerPra@users.noreply.github.com>
2025-06-13 14:08:12 +0200

    Update qaoa example

--- examples/max_cut_qaoa.py
+++ examples/max_cut_qaoa.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     qc = build_qaoa_circuit(graph, layers, cost_layers, mixer_layers)
     id = f"{n_qubits}_{method}"
 
-    if method not in ['scipy', 'polypus_cunqa']:
+    if method not in ['scipy', 'polypus_local', 'polypus_cunqa']:
         raise ValueError("Method must be one of: 'scipy', 'polypus_local', 'polypus_cunqa'")
 
     if method == 'scipy':
@@ -185,6 +185,75 @@ if __name__ == "__main__":
         plt.suptitle(f"Expected MaxCut value: {BEST_SOLUTION} - Runned DE with {POPULATION_SIZE} individuals for {MAX_GENERATIONS} generations for a QAOA with {layers} layers")
         plt.tight_layout(rect=[0, 0, 1, 0.95])
         plt.savefig(f"examples/maxcut_result_{id}.png")
+
+    elif method == 'polypus_local':
+
+        import polypus
+        # Qraise
+        N_QPUS = POPULATION_SIZE
+        if N_QPUS > 64:
+            N_QPUS = 64
+
+        N = math.ceil(N_QPUS / 32)
+
+        # Run Polypus with cunqa infrastructure
+        tic = time.time()
+        result_cunqa = polypus.differential_evolution(
+            qc=qc, 
+            shots=N_SHOTS, 
+            n_qpus = N_QPUS, 
+            expectation_function=bitstring_to_obj, 
+            generations=MAX_GENERATIONS, 
+            population_size=POPULATION_SIZE, 
+            dimensions=2*layers, 
+            infraestructure="local", 
+            id=id,
+            tolerance=TOL,
+            nodes=N)
+        polypus_time_cunqa = time.time() - tic
+
+        qc = build_qaoa_circuit(graph, layers, cost_layers, mixer_layers)
+        backend = AerSimulator()
+        qc.assign_parameters(result_cunqa, inplace=True)
+        result_poly = backend.run(qc, shots=N_SHOTS).result()
+        counts_poly = result_poly.get_counts()
+
+        best_bitstring_poly = max(counts_poly, key=counts_poly.get)
+        cut_poly = bitstring_to_obj(best_bitstring_poly)
+
+        # Plots
+        pos = nx.spring_layout(graph, seed=42)
+        fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+
+        # 1. Original graph
+        nx.draw(graph, pos, ax=axes[0], with_labels=True, node_color='lightgray', edge_color='black', node_size=800, font_color='black')
+        axes[0].set_title("Original Graph")
+
+        # 2. Polypus 
+        color_map_poly = ['red' if best_bitstring_poly[i] == '0' else 'blue' for i in range(n_qubits)]
+        nx.draw(graph, pos, ax=axes[1], with_labels=True,node_color=color_map_poly, node_size=800, font_color='white')
+        cut_edges_poly = [(i, j) for i, j in graph.edges() if best_bitstring_poly[i] != best_bitstring_poly[j]]
+        nx.draw_networkx_edges(
+            graph, pos, ax=axes[1], edgelist=cut_edges_poly,
+            width=4, edge_color='yellow'
+        )
+        axes[1].set_title(f"Polypus QAOA Cut: {cut_poly} - Time: {polypus_time_cunqa:.2f}s")
+
+        # 3. Counts distribution
+        axes[2].bar(counts_poly.keys(), counts_poly.values(), color='blue')
+        axes[2].set_xlabel('Bitstring')
+        axes[2].set_ylabel('Counts')
+        axes[2].set_title('Counts Distribution')
+        plt.xticks(rotation=90)
+        plt.subplots_adjust(bottom=0.2)
+
+        plt.suptitle(f"Expected MaxCut value: {BEST_SOLUTION} - Runned DE with {POPULATION_SIZE} individuals for {MAX_GENERATIONS} generations for a QAOA with {layers} layers")
+        plt.tight_layout(rect=[0, 0, 1, 0.95])
+        plt.savefig(f"examples/maxcut_result_{id}.png")
+
+        mean_energy = compute_mean_energy(counts_poly)
+        approximation_ratio_polypus_cunqa = mean_energy / BEST_SOLUTION
+        log_experiment_results(n_qubits, 'polypus_local', polypus_time_cunqa, approximation_ratio_polypus_cunqa, N_SHOTS)
 
     elif method == 'polypus_cunqa':
 
