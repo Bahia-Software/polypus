87d858b484aedbf6f018f86ffe27d21680d30a5d
DBFerPra <diegoferpra@gmail.com>
2025-10-11 00:19:54 +0200

    New version for differential evolution

--- tests/running_quantum_circuits_local.rs
+++ tests/running_quantum_circuits_local.rs
@@ -1,7 +1,17 @@
-use polypus::algorithms::AlgorithmSingleRun;
+use polypus::algorithms::{AlgorithmSingleRun,AlgorithmDifferentialEvolution};
 use polypus::AlgorithmTrait;
 
 #[test]
 fn run_single_run() {    
     AlgorithmSingleRun.description();
+}
+
+// #[test]
+// fn run_distribute_by_shots() {    
+//     AlgorithmDistributeByShots.description();
+// }
+
+#[test]
+fn run_differential_evolution() {    
+    AlgorithmDifferentialEvolution.description();
 }
