6f5fe95a5777ab61bc66161a0b0b83d5b0dfb5b7
DBFerPra <diegoferpra@gmail.com>
2025-10-16 00:25:56 +0200

    Small error correction in parameters for cunqa

--- examples/max_cut_qaoa.py
+++ examples/max_cut_qaoa.py
@@ -275,7 +275,7 @@ if __name__ == "__main__":
             generations=MAX_GENERATIONS, 
             population_size=POPULATION_SIZE, 
             dimensions=2*layers, 
-            infraestructure="qmio", 
+            infrastructure="qmio", 
             id=id,
             tolerance=TOL,
             nodes=N)
